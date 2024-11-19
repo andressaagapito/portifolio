794e1af99c1fc31ba111627a1bcfcd68b1892cfa
Andressa Conceição da Silva <andressaagapito@live.com>
2024-11-19 00:24:14 -0300

    responsividade portifolio

--- src/components/Portifolio/Portifolio.jsx
+++ src/components/Portifolio/Portifolio.jsx
@@ -1,4 +1,3 @@
-// Componente Portifolio
 import React from "react";
 import greenstar from "../../assets/mural/greenstar.png";
 import asterisk from "../../assets/mural/asterisk.png";
@@ -33,8 +32,8 @@ function Portifolio() {
       </div>
 
       {/* Grid de Cards */}
-      <div className="flex flex-wrap justify-center items-center bg-[#efe3d4] w-full h-[80vh] p-4">
-        <div className="grid grid-cols-2 gap-20">
+      <div className="flex flex-wrap justify-center items-center bg-[#efe3d4] w-full h-[80vh] p-4 overflow-hidden">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl mx-auto">
           {/* Cards do Weather App */}
           <Card
             text="WeatherApp"
@@ -69,7 +68,7 @@ function Card({ text, color, image, captionColor, icon, link }) {
       className="flex flex-col items-center no-underline">
       {/* Card com a imagem inteira dentro */}
       <div
-        className={`relative flex items-center justify-center w-40 h-40 ${color} shadow-lg transform transition-transform duration-300 hover:scale-110 cursor-pointer`}>
+        className={`relative flex items-center justify-center w-32 h-32 sm:w-40 sm:h-40 ${color} shadow-lg transform transition-transform duration-300 hover:scale-110 cursor-pointer`}>
         {/* Pino decorativo no topo do card */}
         <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1 w-4 h-4 bg-gray-500 rounded-full shadow-md z-10"></div>
 
@@ -83,12 +82,12 @@ function Card({ text, color, image, captionColor, icon, link }) {
 
       {/* Legenda abaixo do card com fundo sólido retangular e cor personalizada */}
       <div
-        className={`mt-3 ${captionColor} w-full text-center flex items-center justify-center gap-2`}>
+        className={`mt-3 ${captionColor} w-full text-center font-jetbrains flex items-center justify-center gap-2`}>
         {/* Texto */}
-        <p className="text-white font-bold py-1">{text}</p>
+        <p className="font-bold py-1">{text}</p>
         {/* Ícones */}
         {icon && (
-          <div className="flex items-center gap-1 text-white">
+          <div className="flex items-center gap-1">
             {Array.isArray(icon) ? (
               icon.map((item, index) => <span key={index}>{item}</span>)
             ) : (
